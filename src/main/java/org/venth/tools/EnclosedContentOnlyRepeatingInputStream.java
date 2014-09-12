94456f7c50e60b29371b35a250cb771317d4a50d
venth <artur.krysiak.warszawa@gmail.com>
2014-09-12 17:32:22 +0200

    implemented enclosing content only repeating stream

--- src/main/java/org/venth/tools/EnclosedContentOnlyRepeatingInputStream.java
+++ src/main/java/org/venth/tools/EnclosedContentOnlyRepeatingInputStream.java
@@ -1,7 +1,9 @@
 package org.venth.tools;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.SequenceInputStream;
 
 /**
  * this class repeats only a content which is provided
@@ -10,18 +12,20 @@ import java.io.InputStream;
  * So the results stream will be looks like that:
  * head, content .. until sizeLimit is reached, tail
  *
- * This class is closely collaborating with {@link org.venth.tools.RepeatableInputStream}
+ * This class is closely collaborating with {@link RepeatingInputStream}
  * to provide repeatable streaming capabilities
  *
  * @author Artur Krysiak (last modified by $Author$).
  * @version $Revision$ $Date$
  */
 public class EnclosedContentOnlyRepeatingInputStream extends InputStream {
+    private final SequenceInputStream enclosedStream;
+
     /**
      * Creates a stream that will first stream a head, then repeat the content and after
      * the size limit is reached, the stream will provide the tail
      *
-     * this constructor is only a facade to {@link #EnclosedContentOnlyRepeatingInputStream(byte[], RepeatableInputStream, byte[])}
+     * this constructor is only a facade to {@link #EnclosedContentOnlyRepeatingInputStream(byte[], RepeatingInputStream, byte[])}
      *
      * @param head a head to stream before the content
      * @param content a content to be repeated until size limit is reached
@@ -29,17 +33,20 @@ public class EnclosedContentOnlyRepeatingInputStream extends InputStream {
      * @param sizeLimit a content's size limit in bytes
      */
     public EnclosedContentOnlyRepeatingInputStream(byte[] head, byte[] content, byte[] tail, long sizeLimit) {
-        this(head, new RepeatableInputStream(content, sizeLimit), tail);
+        this(head, new RepeatingInputStream(content, sizeLimit), tail);
     }
 
 
-    public EnclosedContentOnlyRepeatingInputStream(byte[] head, RepeatableInputStream repeatingContentStream, byte[] tail) {
-
+    public EnclosedContentOnlyRepeatingInputStream(byte[] head, RepeatingInputStream repeatingContentStream, byte[] tail) {
+        enclosedStream = new SequenceInputStream(
+                new SequenceInputStream(new ByteArrayInputStream(head), repeatingContentStream),
+                new ByteArrayInputStream(tail)
+        );
     }
 
 
     @Override
     public int read() throws IOException {
-        return 0;
+        return enclosedStream.read();
     }
 }
--- src/test/java/org/venth/tools/EnclosedContentOnlyRepeatingInputStreamTest.groovy
+++ src/test/java/org/venth/tools/EnclosedContentOnlyRepeatingInputStreamTest.groovy
@@ -1,5 +1,8 @@
 package org.venth.tools
 
+import com.google.common.collect.ComparisonChain
+import com.google.common.collect.ObjectArrays
+import com.google.common.collect.Ordering
 import spock.lang.Specification
 
 /**
@@ -11,20 +14,111 @@ class EnclosedContentOnlyRepeatingInputStreamTest extends Specification {
         given: 'a head'
         def head = "Yoda".bytes
 
-        and: 'content stream'
-        def conentStream = Mock RepeatableInputStream
+        and: 'content stream returns one byte'
+        def contentStream = Mock RepeatingInputStream
+        contentStream.read() >>> [ 32, -1 ]
 
         and: 'a tail'
         def tail = "Master".bytes
 
-        and: 'a content'
+        and: 'the enclosed stream'
+        def enclosedStream = new EnclosedContentOnlyRepeatingInputStream(head, contentStream, tail)
 
-        when:
+        when: 'all the stream is read'
+        def wholeStream = getWholeStreamContent(enclosedStream)
 
+        then: 'read stream is bigger than the head'
+        wholeStream.length > head.length
+
+        then: 'head was read as first'
+        0 == ComparisonChain.start()
+            .compare(Arrays.copyOf(wholeStream, head.length), head, Ordering.<Byte>allEqual())
+            .result()
     }
 
     def "tail is streamed after the content's stream"() {
+        given: 'a head'
+        def head = "Yoda".bytes
+
+        and: 'content stream returns one byte'
+        def contentStream = Mock RepeatingInputStream
+        contentStream.read() >>> [32, -1]
+
+        and: 'a tail'
+        def tail = "Master".bytes
+
+        and: 'the enclosed stream'
+        def enclosedStream = new EnclosedContentOnlyRepeatingInputStream(head, contentStream, tail)
+
+        when: 'all the stream is read'
+        def wholeStream = getWholeStreamContent(enclosedStream)
+
+        then: 'read stream is bigger than the head and tail'
+        wholeStream.length > (head.length + tail.length)
+
+        then: 'tail was read at the end'
+        def readTail = new byte[tail.length]
+        //to satisfy spock ;)
+        null == System.arraycopy(wholeStream, wholeStream.length - tail.length - 1, readTail, 0, tail.length)
+
+        0 == ComparisonChain.start()
+                .compare(readTail, tail, Ordering.<Byte>allEqual())
+                .result()
+    }
+
+    def "content is provided between head and tail"() {
+        given: 'a head'
+        def head = "Yoda".bytes
+
+        and: 'content stream returns one byte'
+        def contentStream = Mock RepeatingInputStream
+        contentStream.read() >>> [32, -1]
+
+        and: 'a tail'
+        def tail = "Master".bytes
+
+        and: 'the enclosed stream'
+        def enclosedStream = new EnclosedContentOnlyRepeatingInputStream(head, contentStream, tail)
+
+        when: 'all the stream is read'
+        def wholeStream = getWholeStreamContent(enclosedStream)
+
+        then: 'read stream size equals sum of length of head, content and the tail'
+        wholeStream.length == (head.length + 1 + tail.length)
+
+        then: 'exactly one byte of content was read'
+        wholeStream[head.length] == 32
+    }
+
+    def "for empty content stream only head and tail are streamed"() {
+        given: 'a head'
+        def head = "Yoda".bytes
+
+        and: 'content stream is empty'
+        def contentStream = Mock RepeatingInputStream
+        contentStream.read() >> -1
+
+        and: 'a tail'
+        def tail = "Master".bytes
+
+        and: 'the enclosed stream'
+        def enclosedStream = new EnclosedContentOnlyRepeatingInputStream(head, contentStream, tail)
+
+        when: 'all the stream is read'
+        def wholeStream = getWholeStreamContent(enclosedStream)
+
+        then: 'read stream size equals sum of length of head and the tail'
+        wholeStream.length == (head.length + tail.length)
+    }
 
 
+    byte[] getWholeStreamContent(EnclosedContentOnlyRepeatingInputStream enclosedStream) {
+        def readContent = []
+        def byte element
+        while ((element = enclosedStream.read()) != -1) {
+            readContent = readContent + element
+        }
+
+        readContent.toArray(new byte[0])
     }
 }
